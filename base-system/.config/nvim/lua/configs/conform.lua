0b9c7fe5d077d7e409488d7a8e9a9fa94ed46ffe
Martin Ellegård <martin.ellegard@gmail.com>
2025-02-20 12:27:15 +0100

    divided into systems

---
+++ base-system/.config/nvim/lua/configs/conform.lua
@@ -0,0 +1,21 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettierd" },
+    html = { "prettierd" },
+    js = { "prettierd" },
+    ts = { "prettierd" },
+    javascript = { "prettierd" },
+    javascriptreact = { "prettierd" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
